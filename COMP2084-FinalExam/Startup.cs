4b9b202cc6e4082714a17bd7e098d4c810f9f7ee
Esat IBIS <esat.taha.ibis@outlook.com>
2016-12-13 08:04:57 -0500

    Add project files.

---
+++ COMP2084-FinalExam/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(COMP2084_FinalExam.Startup))]
+namespace COMP2084_FinalExam
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
